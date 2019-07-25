fda9abba2c40fbf43ae42cb31b1a6e02bfc90727
mozart98 <mozart.assila@gmail.com>
2019-07-25 14:33:03 +0100

    Adding a state to the App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React ,{Component} from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state={
+    todos :[
+      {
+        id : 1,
+        title : 'take out the trash',
+        completed : false
+
+      },
+      {
+        id : 2,
+        title : 'Dinner with wife',
+        completed : false
+
+      },
+      {
+        id : 3,
+        title : 'Meeting with boss',
+        completed : false
+
+      }
+    ]
+  }
+
+  render() {
+    console.log(this.state.todos)
+    return(
     <div className="App">
-      <h1>App</h1>
+      <Todos/>
     </div>
   );
+}
 }
 
 export default App;
