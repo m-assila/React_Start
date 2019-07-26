d8274021cef0ac708673fe692dbe0e8dccdc9ebe
mozart98 <mozart.assila@gmail.com>
2019-07-26 14:56:07 +0100

    Use variable for styling

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types';
 export default class Todoitem extends Component {
     render() {
         return (
-            <div style={{backgroundColor :'#f4f4f4'}}>
+            <div style={ItemStyle}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
     }
+}
+
+//Use a variable for styling
+
+const ItemStyle={
+    backgroundColor :'#f4f4f4'
 }
 
 Todoitem.propTypes={
