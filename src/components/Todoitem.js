22cd12ff8881faf137761ce85b5561996b810a02
mozart98 <mozart.assila@gmail.com>
2019-07-26 15:10:05 +0100

    Dynamic Styling :using function

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Todoitem extends Component {
+    getStyle =()=>{
+        if(this.props.todo.completed){
+            return{
+                textDecoration : 'line-through'
+            }
+        }
+        else{
+            return{
+                textDecoration : 'none'
+            }
+        }
+    }
+
     render() {
         return (
-            <div style={ItemStyle}>
+            <div style={this.getStyle()}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
     }
 }
 
-//Use a variable for styling
 
-const ItemStyle={
-    backgroundColor :'#f4f4f4'
-}
 
 Todoitem.propTypes={
     todo : PropTypes.object.isRequired
